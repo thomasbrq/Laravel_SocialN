9f4f80140e5b2f899f196d9e3525d1e17af08917
Thomas Brq <71637888+thomasbrq@users.noreply.github.com>
2021-02-01 11:29:45 +0100

    Dashboard redirect

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,8 +12,8 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-jet-nav-link href="{{ route('post.index') }}" :active="request()->routeIs('post.index')">
+                        Home
                     </x-jet-nav-link>
                 </div>
             </div>
