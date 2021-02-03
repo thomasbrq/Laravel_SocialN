4e022bf9e1cf3daf690f0b8f17d22e3e42f4ddd2
Thomas Brq <71637888+thomasbrq@users.noreply.github.com>
2021-02-03 14:54:10 +0100

    button design

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Post')
 
 @section('sidebar')
-    <link rel="stylesheet" href="{{ asset('css/show.css') }}">
     @parent
+    <link rel="stylesheet" href="{{ asset('css/show.css') }}">
 @endsection
 
 @section('content')
@@ -34,14 +34,14 @@
 
             @if (auth()->user() && auth()->user()->id == $post->author)
                 <form action="{{ route('post.edit', [$post->slug, $post->id]) }}">
-                    <button type="submit">
+                    <button type="submit" class="btn edelete">
                         <svg class="w-6 h-6 edit-svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                     </button>
                 </form>
 
-                <button type="button" class="btn" data-toggle="modal" data-target="#exampleModalCenter">
+                <button type="button" class="btn edelete" data-toggle="modal" data-target="#exampleModalCenter">
                     <svg class="w-6 h-6 svg-close" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                  </button>
+                </button>
                   
                   <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                     <div class="modal-dialog" role="document">
