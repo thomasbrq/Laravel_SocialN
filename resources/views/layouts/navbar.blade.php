02b16b331a36b279ae45979247370d8780356ea5
Thomas Brq <71637888+thomasbrq@users.noreply.github.com>
2021-02-08 08:56:31 +0100

    Script JS

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -61,9 +61,7 @@
     @show
 
     <div class="container">
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-        </script>
+        <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
             integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
         </script>
@@ -74,8 +72,6 @@
         </script>
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
         @yield('content')
-
-
     </div>
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,9 +61,7 @@
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/infinite-scroll@4/dist/infinite-scroll.pkgd.min.js"></script>
+    <script src="{{ asset('js/infinite-scroll.min.js') }}"></script>
     <script>
         $('.scrolling-pagination').infiniteScroll({
             // options
