3f9968d015eb467da276d8cc12ca9facf1b68ae5
Thomas Brq <71637888+thomasbrq@users.noreply.github.com>
2021-02-10 13:33:28 +0100

    Ajout tabs

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -16,24 +16,43 @@
     <div class="message">
         <form action="{{ route('post.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
-            <input type="text" name="author" id="author" value="{{ auth()->user()->id }}" class="hider">
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title" maxlength="100">
+            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link active" id="pills-post-tab" data-bs-toggle="pill" href="#pills-post" role="tab" aria-controls="pills-post" aria-selected="true">Post</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-picture-tab" data-bs-toggle="pill" href="#pills-picture" role="tab" aria-controls="pills-picture" aria-selected="false">Picture</a>
+                  </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link" id="pills-website-tab" data-bs-toggle="pill" href="#pills-website" role="tab" aria-controls="pills-website" aria-selected="false">Website link</a>
+                </li>
+              </ul>
+              <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-post" role="tabpanel" aria-labelledby="pills-post-tab">
+                    <input type="text" name="author" id="author" value="{{ auth()->user()->id }}" class="hider">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" id="title" maxlength="100">
 
-            <div class="loading-bar">
-                <div class="ldBarTitle" data-value="0" id="ldBarTitle" data-max='100' data-preset="circle" style="width: 25px;height: 25px">
+                    <div class="loading-bar">
+                        <div class="ldBarTitle" data-value="0" id="ldBarTitle" data-max='100' data-preset="circle" style="width: 25px;height: 25px">
+                        </div>
+                    </div>
+
+                    <label for="description">Description</label>
+                    <textarea name="description" id="description" cols="30" rows="10" maxlength="500"></textarea>
+                    
+                    <div class="loading-bar">
+                        <div class="ldBarDescription" data-value="0" id="ldBarDescription" data-max='500' data-preset="circle" style="width: 25px;height: 25px">
+                        </div>
+                    </div>
                 </div>
-            </div>
-
-            <input type="file" name="picture" id="picture">
-
-            <label for="description">Description</label>
-            <textarea name="description" id="description" cols="30" rows="10" maxlength="500"></textarea>
-            
-            <div class="loading-bar">
-                <div class="ldBarDescription" data-value="0" id="ldBarDescription" data-max='500' data-preset="circle" style="width: 25px;height: 25px">
+                <div class="tab-pane fade" id="pills-picture" role="tabpanel" aria-labelledby="pills-picture-tab">
+                    <input type="file" name="picture" id="picture">
                 </div>
-            </div>
+                <div class="tab-pane fade" id="pills-website" role="tabpanel" aria-labelledby="pills-website-tab">
+                    
+                </div>
+              </div>
 
             <button type="submit">Envoyer</button>
         </form>
